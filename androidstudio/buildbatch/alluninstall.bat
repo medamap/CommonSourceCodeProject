99a414fd10e7fa28736734470c2152b81e1a0aba
Medamap <junmera@gmail.com>
2024-03-23 20:33:45 +0900

    端末からアプリケーションを削除するバッチを追加

---
+++ androidstudio/buildbatch/alluninstall.bat
@@ -0,0 +1,185 @@
+@echo off
+setlocal
+
+echo ********** X1 **********
+adb uninstall com.shikarunochi.emulator.x1
+
+echo ********** X1turbo **********
+adb uninstall com.shikarunochi.emulator.x1turbo
+
+echo ********** X1turboZ **********
+adb uninstall com.shikarunochi.emulator.x1turboz
+
+echo ********** X1Twin **********
+adb uninstall com.shikarunochi.emulator.x1twin
+
+echo ********** MZ80B **********
+adb uninstall com.shikarunochi.emulator.mz80b
+
+echo ********** MZ80K **********
+adb uninstall com.shikarunochi.emulator.mz80k
+
+echo ********** MZ700 **********
+adb uninstall com.shikarunochi.emulator.mz700
+
+echo ********** MZ800 **********
+adb uninstall com.shikarunochi.emulator.mz800
+
+echo ********** MZ1500 **********
+adb uninstall com.shikarunochi.emulator.mz1500
+
+echo ********** MZ2200 **********
+adb uninstall com.shikarunochi.emulator.mz2200
+
+echo ********** MZ2500 **********
+adb uninstall com.shikarunochi.emulator.mz2500
+
+echo ********** PC6001 **********
+adb uninstall com.shikarunochi.emulator.pc6001
+
+echo ********** PC6601 **********
+adb uninstall com.shikarunochi.emulator.pc6601
+
+echo ********** PC6601SR **********
+adb uninstall com.shikarunochi.emulator.pc6601sr
+
+echo ********** PC8001 **********
+adb uninstall com.shikarunochi.emulator.pc8001
+
+echo ********** PC8801MA **********
+adb uninstall com.shikarunochi.emulator.pc8801ma
+
+echo ********** MSX **********
+adb uninstall com.shikarunochi.emulator.msx
+
+echo ********** MSX2 **********
+adb uninstall com.shikarunochi.emulator.msx2
+
+echo ********** MSX2+ **********
+adb uninstall com.shikarunochi.emulator.msx2p
+
+echo ********** FM7 **********
+adb uninstall com.shikarunochi.emulator.fm7
+
+echo ********** FM8 **********
+adb uninstall com.shikarunochi.emulator.fm8
+
+echo ********** FM77 **********
+adb uninstall com.shikarunochi.emulator.fm77
+
+echo ********** FM77L4 **********
+adb uninstall com.shikarunochi.emulator.fm77l4
+
+echo ********** FM77AV **********
+adb uninstall com.shikarunochi.emulator.fm77av
+
+echo ********** FM77AV40 **********
+adb uninstall com.shikarunochi.emulator.fm77av40
+
+echo ********** FM77AV40EX **********
+adb uninstall com.shikarunochi.emulator.fm77av40ex
+
+echo ********** PASOPIA **********
+adb uninstall com.shikarunochi.emulator.pasopia
+
+echo ********** PASOPIA7 **********
+adb uninstall com.shikarunochi.emulator.pasopia7
+
+echo ********** PYUTA **********
+adb uninstall com.shikarunochi.emulator.pyuta
+
+echo ********** RX78 **********
+adb uninstall com.shikarunochi.emulator.rx78
+
+echo ********** SMC70 **********
+adb uninstall com.shikarunochi.emulator.smc70
+
+echo ********** SMC777 **********
+adb uninstall com.shikarunochi.emulator.smc777
+
+echo ********** JR100 **********
+adb uninstall com.shikarunochi.emulator.jr100
+
+echo ********** JR800 **********
+adb uninstall com.shikarunochi.emulator.jr800
+
+echo ********** M5 **********
+adb uninstall com.shikarunochi.emulator.m5
+
+echo ********** M23 **********
+adb uninstall com.shikarunochi.emulator.m23
+
+echo ********** M68 **********
+adb uninstall com.shikarunochi.emulator.m68
+
+echo ********** SC3000 **********
+adb uninstall com.shikarunochi.emulator.sc3000
+
+echo ********** TK85 **********
+adb uninstall com.shikarunochi.emulator.tk85
+
+echo ********** MULTUI8 **********
+adb uninstall com.shikarunochi.emulator.multi8
+
+echo ********** MycomZ80A **********
+adb uninstall com.shikarunochi.emulator.mycomz80a
+
+echo ********** FamilyBasic **********
+adb uninstall com.shikarunochi.emulator.familybasic
+
+echo ********** GameGear **********
+adb uninstall com.shikarunochi.emulator.gamegear
+
+echo ********** MasterSystem **********
+adb uninstall com.shikarunochi.emulator.mastersystem
+
+echo ********** PC-Engine **********
+adb uninstall com.shikarunochi.emulator.pcengine
+
+echo ********** PC-9801 **********
+adb uninstall com.shikarunochi.emulator.pc9801
+
+echo ********** PC-9801E **********
+adb uninstall com.shikarunochi.emulator.pc9801e
+
+echo ********** PC-9801RA **********
+adb uninstall com.shikarunochi.emulator.pc9801ra
+
+echo ********** PC-9801U **********
+adb uninstall com.shikarunochi.emulator.pc9801u
+
+echo ********** PC-9801VF **********
+adb uninstall com.shikarunochi.emulator.pc9801vf
+
+echo ********** PC-9801VM **********
+adb uninstall com.shikarunochi.emulator.pc9801vm
+
+echo ********** PC-9801VX **********
+adb uninstall com.shikarunochi.emulator.pc9801vx
+
+echo ********** PC-98 Do **********
+adb uninstall com.shikarunochi.emulator.pc98do
+
+echo ********** PC-98HA **********
+adb uninstall com.shikarunochi.emulator.pc98ha
+
+echo ********** PC-98LT **********
+adb uninstall com.shikarunochi.emulator.pc98lt
+
+echo ********** PC-98RL **********
+adb uninstall com.shikarunochi.emulator.pc98rl
+
+echo ********** PC-98XA **********
+adb uninstall com.shikarunochi.emulator.pc98xa
+
+echo ********** PC-98XL **********
+adb uninstall com.shikarunochi.emulator.pc98xl
+
+echo ********** N5200 **********
+adb uninstall com.shikarunochi.emulator.n5200
+
+:: 以降の処理をここに記述できます
+:: 例:
+echo ビルドプロセスが完了しました。
+
+endlocal
