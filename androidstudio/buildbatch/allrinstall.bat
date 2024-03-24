204e8d9bcd20aea9a0d4cfe59ad545c381a3a06a
Medamap <junmera@gmail.com>
2024-03-24 21:51:19 +0900

    一括インストールバッチを追加

---
+++ androidstudio/buildbatch/allrinstall.bat
@@ -0,0 +1,203 @@
+@echo off
+setlocal
+
+echo ********** X1 **********
+call rinstall.bat x1
+
+echo ********** X1turbo **********
+call rinstall.bat x1turbo
+
+echo ********** X1turboZ **********
+call rinstall.bat x1turboz
+
+echo ********** X1Twin **********
+call rinstall.bat x1twin
+
+echo ********** MZ80B **********
+call rinstall.bat mz80b
+
+echo ********** MZ80K **********
+call rinstall.bat mz80k
+
+echo ********** MZ700 **********
+call rinstall.bat mz700
+
+echo ********** MZ800 **********
+call rinstall.bat mz800
+
+echo ********** MZ1500 **********
+call rinstall.bat mz1500
+
+echo ********** MZ2200 **********
+call rinstall.bat mz2200
+
+echo ********** MZ2500 **********
+call rinstall.bat mz2500
+
+echo ********** PC6001 **********
+call rinstall.bat pc6001
+
+echo ********** PC100 **********
+call rinstall.bat pc100
+
+echo ********** PC2001 **********
+call rinstall.bat pc2001
+
+echo ********** PC6601 **********
+call rinstall.bat pc6601
+
+echo ********** PC6601SR **********
+call rinstall.bat pc6601sr
+
+echo ********** PC8001 **********
+call rinstall.bat pc8001
+
+echo ********** PC8001mk2 **********
+call rinstall.bat pc8001mk2
+
+echo ********** PC8201 **********
+call rinstall.bat pc8201
+
+echo ********** PC8801 **********
+call rinstall.bat pc8801
+
+echo ********** PC8801MA **********
+call rinstall.bat pc8801ma
+
+echo ********** PC8801mk2 **********
+call rinstall.bat pc8801mk2
+
+echo ********** MSX **********
+call rinstall.bat msx
+
+echo ********** MSX2 **********
+call rinstall.bat msx2
+
+echo ********** MSX2+ **********
+call rinstall.bat msx2p
+
+echo ********** FM7 **********
+call rinstall.bat fm7
+
+echo ********** FM8 **********
+call rinstall.bat fm8
+
+echo ********** FM77 **********
+call rinstall.bat fm77
+
+echo ********** FM77L4 **********
+call rinstall.bat fm77l4
+
+echo ********** FM77AV **********
+call rinstall.bat fm77av
+
+echo ********** FM77AV40 **********
+call rinstall.bat fm77av40
+
+echo ********** FM77AV40EX **********
+call rinstall.bat fm77av40ex
+
+echo ********** PASOPIA **********
+call rinstall.bat pasopia
+
+echo ********** PASOPIA7 **********
+call rinstall.bat pasopia7
+
+echo ********** PYUTA **********
+call rinstall.bat pyuta
+
+echo ********** RX78 **********
+call rinstall.bat rx78
+
+echo ********** SMC70 **********
+call rinstall.bat smc70
+
+echo ********** SMC777 **********
+call rinstall.bat smc777
+
+echo ********** JR100 **********
+call rinstall.bat jr100
+
+echo ********** JR800 **********
+call rinstall.bat jr800
+
+echo ********** M5 **********
+call rinstall.bat m5
+
+echo ********** M23 **********
+call rinstall.bat m23
+
+echo ********** M68 **********
+call rinstall.bat m68
+
+echo ********** SC3000 **********
+call rinstall.bat sc3000
+
+echo ********** TK85 **********
+call rinstall.bat tk85
+
+echo ********** MULTUI8 **********
+call rinstall.bat multi8
+
+echo ********** MycomZ80A **********
+call rinstall.bat mycomz80a
+
+echo ********** FamilyBasic **********
+call rinstall.bat familybasic
+
+echo ********** GameGear **********
+call rinstall.bat gamegear
+
+echo ********** MasterSystem **********
+call rinstall.bat mastersystem
+
+echo ********** PC-Engine **********
+call rinstall.bat pcengine
+
+echo ********** PC-9801 **********
+call rinstall.bat pc9801
+
+echo ********** PC-9801E **********
+call rinstall.bat pc9801e
+
+echo ********** PC-9801RA **********
+call rinstall.bat pc9801ra
+
+echo ********** PC-9801U **********
+call rinstall.bat pc9801u
+
+echo ********** PC-9801VF **********
+call rinstall.bat pc9801vf
+
+echo ********** PC-9801VM **********
+call rinstall.bat pc9801vm
+
+echo ********** PC-9801VX **********
+call rinstall.bat pc9801vx
+
+echo ********** PC-98 Do **********
+call rinstall.bat pc98do
+
+echo ********** PC-98HA **********
+call rinstall.bat pc98ha
+
+echo ********** PC-98LT **********
+call rinstall.bat pc98lt
+
+echo ********** PC-98RL **********
+call rinstall.bat pc98rl
+
+echo ********** PC-98XA **********
+call rinstall.bat pc98xa
+
+echo ********** PC-98XL **********
+call rinstall.bat pc98xl
+
+echo ********** N5200 **********
+call rinstall.bat n5200
+
+:: 以降の処理をここに記述できます
+:: 例:
+echo ビルドプロセスが完了しました。
+
+endlocal
---
+++ androidstudio/buildbatch/rinstall.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: 引数を取得し、大文字に変換
+set variant=%1
+set variant_uppercase=%variant%
+for %%i in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do (
+    call set variant_uppercase=%%variant_uppercase:%%i=%%i%%
+)
+
+:: Releaseを付けて完全なビルドタイプを作成
+set buildType=!variant_uppercase!Release
+
+cd ..
+
+:: インストール
+call .\gradlew install!buildType!
+
+cd buildbatch
+
+endlocal
